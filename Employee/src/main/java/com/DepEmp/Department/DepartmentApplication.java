d8748c6a0278cc69fe09f073310f354ece9bda49
chaithragr409@gmail.com <your.email@example.com>
2023-11-06 14:17:03 +0530

    micro modification

---
+++ Employee/src/main/java/com/DepEmp/Department/DepartmentApplication.java
@@ -0,0 +1,16 @@
+package com.DepEmp.Department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentApplication {
+	class  C{
+
+	};
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentApplication.class, args);
+	}
+
+}
---
+++ Employee/src/main/java/com/DepEmp/Department/Entity/Department.java
@@ -0,0 +1,61 @@
+package com.DepEmp.Department.Entity;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "departments")
+public class Department {
+    @Id
+    private String departmentId;
+
+    private String departmentName;
+
+    @Field(name = "created_at")
+    private Timestamp createdAt;
+
+    @Field(name = "updated_at")
+    private Timestamp updatedAt;
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
